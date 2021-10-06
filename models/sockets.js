69684d89cd97028179fa4ad536a400f575ab223f
Luis Carrion <lcarrionalvarez@gmail.com>
2021-10-05 21:47:46 -0500

    borrar, editar, votar bandas

--- models/sockets.js
+++ models/sockets.js
@@ -13,7 +13,25 @@ class Sockets {
       console.log("Cliente conectado");
 
       // Emitir al cliente conectado las bandas actuales
-        socket.emit('current-bands', this.bandList.getBands() );
+      socket.emit('current-bands', this.bandList.getBands() );
+
+      // Votar por la banda
+      socket.on('votar-banda', (id) => {
+        this.bandList.increaseVotes(id);        
+        this.io.emit('current-bands', this.bandList.getBands() );// se vuelve a emitir debido a que si no secoloca esta instruccion hay que recargar el navegador para que se pueda interpretar
+      })
+
+      // Borrar bandas
+      socket.on('borrar-banda', (id) => {
+        this.bandList.removeBand(id)
+        this.io.emit('current-bands', this.bandList.getBands() );
+      })
+
+      //Cambiar nombre
+      socket.on('cambiar-nombre-banda', band => {    
+        this.bandList.changeName(band.id, band.nombre);
+        this.io.emit('current-bands', this.bandList.getBands() );
+      })
     });
   }
 }
